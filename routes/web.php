4c10ed63df7309cc58ea1834fd7da3f54b570cf1
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-02-12 19:41:44 +0000

    Push de prueba para Danny

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd(phpversion());
+    dd(phpversion(),"peruano");
 });
 
 Route::get('/dashboard', function () {
