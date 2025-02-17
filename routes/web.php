6fd3f0edf79b08718c60222a2574c5754e4244b5
Danny79k <belloli.danny@gmail.com>
2025-02-17 18:21:37 +0000

    dashboard arreglado; de nada

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    phpversion();
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
