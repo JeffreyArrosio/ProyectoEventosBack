9bd48faa08bb4d2b3c0f4a6071e19ee1ab2801dc
Danny79k <belloli.danny@gmail.com>
2025-02-20 20:53:01 +0000

    feat: trabajando en controladores de relaciones

---
+++ app/Http/Controllers/api/RelationshipControllers/AssociationTypeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\api\RelationshipControllers;
+
+use Orion\Http\Controllers\RelationController;
+use App\Models\Association;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\HasApiTokens;
+use Orion\Concerns\DisableAuthorization;
+
+class AssociationTypeController extends RelationController
+{
+    use HasApiTokens, DisableAuthorization;
+
+    protected $model = Association::class;
+
+    protected $relation = 'type';
+}
+
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\api\AssociationController;
+use App\Http\Controllers\api\RelationshipControllers\AssociationEventController;
 use App\Http\Controllers\api\CommentController;
+use App\Http\Controllers\api\RelationshipControllers\EventAssociationController;
 use App\Http\Controllers\api\EventController;
 use App\Http\Controllers\api\ImageController;
+use App\Http\Controllers\api\RelationshipControllers\AssociationCommentController;
+use App\Http\Controllers\api\RelationshipControllers\AssociationTypeController;
+use App\Http\Controllers\api\RelationshipControllers\AssociationUserController;
+use App\Http\Controllers\api\RelationshipControllers\AssociationUsersController;
 use App\Http\Controllers\api\TypeController;
 use App\Http\Controllers\EventUsersController;
 use Illuminate\Http\Request;
@@ -23,6 +29,19 @@ Route::group(['as' => 'api'], function() {
     Orion::resource('events', EventController::class);
     Orion::resource('images', ImageController::class);
     Orion::resource('types', TypeController::class);
+
+    //relaciones de Association
+
+    Orion::belongsToManyResource('association', 'events', AssociationEventController::class);
+    Orion::belongsToManyResource('association', 'users', AssociationUsersController::class);
+    Orion::belongsToResource('association', 'user', AssociationUserController::class);
+    Orion::hasManyResource('association', 'Comments', AssociationCommentController::class);
+    Orion::belongsToResource('association', 'type', AssociationTypeController::class);
+
+
+    //relaciones de Event
+    Orion::hasManyResource('event', 'associations', EventAssociationController::class);
+    
 
 })->middleware('sanctum');
 
