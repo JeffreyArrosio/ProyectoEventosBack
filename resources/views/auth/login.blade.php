bd79500bc5b717bbdd4fc178e1a92eacc1eb7338
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-02-26 14:33:51 +0000

    feat:inicio de sesión con Google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1250,14 +1250,14 @@
 
                 <!-- Opción de Iniciar Sesión con Google -->
                 <div class="text-center text-gray-500 text-sm mt-4">O inicia sesión con</div>
-                <button class="w-full flex items-center justify-center bg-red-600 hover:bg-red-500 text-white py-2 rounded-lg transition">
+                <x-button-link href="{{ route('auth.google') }}" class="w-full flex items-center justify-center bg-red-600 hover:bg-red-500 text-white py-2 rounded-lg transition cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                         class="bi bi-google mr-2" viewBox="0 0 16 16">
                         <path
                             d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
                     </svg>
                     Iniciar con Google
-                </button>
+                </x-button-link>
 
                 <!-- Link a registro -->
                 <p class="text-center text-sm text-gray-600 mt-4">
